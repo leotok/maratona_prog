c721522995ee47abbe8a408cbcd6f1eb5d1e41e6
leotok <leo.wajnsztok@gmail.com>
2017-05-26 00:33:00 -0300

    conexo

---
+++ URI/conexo.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+casos_teste = int(raw_input())
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z']
+
+for n in xrange(1, casos_teste + 1):
+	print "Case #%d:" %n
+	v, e = map(int, raw_input().split())
+
+	graph = defaultdict(set)
+	for i in xrange(e):
+		x, y = raw_input().split()
+		
+		graph[x].add(y)
+		graph[y].add(x)
+
+	num_conexo = 0
+	visitados = defaultdict(int)
+	
+	for c in abc[:v]:
+		nodes = []
+		if not visitados[c]:
+			nodes.append(c)
+			visitados[c] = 1
+			prox = [c]
+			num_conexo += 1
+			while len(prox) != 0:
+				level = []
+				for node in prox:
+					for vizinho in graph[node]:
+						if not visitados[vizinho]:
+							prox.append(vizinho)
+							level.append(vizinho)
+							nodes.append(vizinho)
+							visitados[vizinho] = 1
+				prox = level
+		if nodes:
+			format_str = ""
+			nodes.sort()
+			for node in nodes:
+				format_str += node + ","
+			print format_str
+
+	print "%d connected components" % num_conexo
+	if n != casos_teste + 1:
+		print ""
+		
+	casos_teste -= 1
