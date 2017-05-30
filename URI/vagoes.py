e0c8bbd2eba5b63e1465deec23d1ba436e93f68b
leotok <leo.wajnsztok@gmail.com>
2017-05-29 23:00:13 -0300

    vagoes

--- URI/vagoes.py
+++ URI/vagoes.py
@@ -15,11 +15,26 @@ def bs(l):
 			break
 	return c
 
+def n(l):
+	i = 0
+	qtd_mov = 0
+
+	while i < len(l):
+		if i + 1 != l[i]:
+			# qtd_mov += 2 * (l[i] - l[l[i] - 1]) - 1
+			qtd_mov += 1
+			l[l[i] - 1], l[i] = l[i], l[l[i] - 1]
+		else:
+			i += 1
+	return qtd_mov
 
 while num_casos:
 
 	tam = int(raw_input())
 	read = map(int, raw_input().split())
+
+	i = 0
+	qtd_mov = 0
 	
-	print 'Optimal train swapping takes %d swaps.' %bs(read)
+	print 'Optimal train swapping takes %d swaps.' % n(read)
 	num_casos -= 1
