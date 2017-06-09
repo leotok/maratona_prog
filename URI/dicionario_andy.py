d50ef984d8d82ef7eb97a64f173fc21230940833
leotok <leo.wajnsztok@gmail.com>
2017-06-08 23:52:24 -0300

    wip dic

--- URI/dicionario_andy.py
+++ URI/dicionario_andy.py
@@ -1,37 +1,43 @@
+# coding: utf8
+
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/1215
 
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z',]
 except_count = 0
 lista = []
-while True:
 
+while True:
 	try:
 		palavras = raw_input().split()
-		raw_input()
-		except_count = 0
 		lista += palavras
 	except:
-		except_count += 1
-
-	if except_count == 2:
 		break
 		
+
 s = set()
 
+lista_2 = []
 for p in lista:
 	p = p.lower()
+	lista_2 += p.split('-')
 
+lista_3 = []
+for p in lista_2:
+	lista_3 += p.split("'")
+
+
+for p in lista_3:
 	for i, c in enumerate(p):
-		if ord(c) < 97 or ord(c) > 123:
+
+		if c not in abc:
 			p = p[:i]
 			break
-		
 
 	s.add(p)
 
-lista = list(s)
-lista.sort()
 
-for l in lista:
+lista_3 = list(s)
+lista_3.sort()
+
+for l in lista_3[:]:
 	print l
-
-# 96 123
