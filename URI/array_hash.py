52a00cfa33992be8475c3ddeb97fdf94d8b978d3
leotok <leo.wajnsztok@gmail.com>
2017-06-23 22:56:45 -0300

    array hash

---
+++ URI/array_hash.py
@@ -0,0 +1,22 @@
+'''
+Array Hash
+URI Online Judge | 1257
+https://www.urionlinejudge.com.br/judge/en/problems/view/1257
+'''
+def array_hash(ls):
+	cl = 0
+	tot = 0
+	for l in ls:
+		for i in range(len(l)):
+			tot += (ord(l[i]) - 65) + cl + i
+		cl += 1
+	return tot
+
+t = int(raw_input())
+for i in range(t):
+	l = int(raw_input())
+	ls = []
+	for j in range(l):
+		ls.append(raw_input())
+	print(array_hash(ls))
+	ls = []
